8f984f9307d8eb88cf8278d59c7268ea36568a11
abhinavak008 <55880867+abhinavak008@users.noreply.github.com>
2021-08-12 14:00:29 +0530

    Update and rename lastCode to lastCode.java

--- lastCode.java
+++ lastCode.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main
+public class lastCode
 {
 	public static void main(String[] args) {
 	    System.out.print("Enter the Number till which U want to print fibonacci series: ");
